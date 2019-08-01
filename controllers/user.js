5dd52b5c059e5ff5d4556dca5c3c669981e9d2eb
Kiran Wattamwar <kiranwattamwar@Kirans-MBP.home>
2019-08-01 00:25:17 -0400

    Remove email validation on name field

--- controllers/user.js
+++ controllers/user.js
@@ -82,7 +82,6 @@ exports.getSignup = (req, res) => {
  */
 exports.postSignup = (req, res, next) => {
   const validationErrors = [];
-  // if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' });
   if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' });
   if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' });
 
@@ -90,7 +89,7 @@ exports.postSignup = (req, res, next) => {
     req.flash('errors', validationErrors);
     return res.redirect('/signup');
   }
-  req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
+  // req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
 
   const user = new User({
     email: req.body.email,

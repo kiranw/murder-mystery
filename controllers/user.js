6414dd11607d03494460fcb807075cc8f9f875c9
Kiran Wattamwar <kiranwattamwar@Kirans-MBP.home>
2019-08-01 18:19:59 -0400

    Update authentication

--- controllers/user.js
+++ controllers/user.js
@@ -34,6 +34,7 @@ exports.postLogin = (req, res, next) => {
     req.flash('errors', validationErrors);
     return res.redirect('/login');
   }
+  // req.body.email = req.body.email.replace(" ","");
   // req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false });
 
   passport.authenticate('local', (err, user, info) => {
